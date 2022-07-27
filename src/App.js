f0c59c482dd30446710b7219c9a456414ece5aaa
Emre Mert <emre.mrt98@gmail.com>
2022-07-28 00:02:14 +0300

    first react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
 import Header from "./header";
 import Footer from "./footer";
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
-  
-  const [inputValue, setInputValue] = useState(""); 
-  const [entryValue, setEntryValue] = useState([]);
-  
+
+  const [inputValue, setInputValue] = useState("");
+  const [array, setArray] = useState([]);
 
   return (
     <div className="App">
       <section className="todoapp">
-      <Header inputValue = {inputValue} setInputValue = {setInputValue} />
+        <Header
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          array={array}
+          setArray={setArray}
+        />
 
         <section className="main">
-        <input className="toggle-all" type="checkbox" />
-          <label for="toggle-all">
+          <input className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">
             Mark all as complete
           </label>
 
@@ -36,13 +40,17 @@ function App() {
                 <button className="destroy"></button>
               </div>
             </li>
-            <li>
-              <div className="view">
-                <input className="toggle" type="checkbox" />
-                <label>Have a life!</label>
-                <button className="destroy"></button>
-              </div>
-            </li>
+            {
+              array.map((element, index) => (
+                <li key={index}>
+                  <div className="view">
+                    <input className="toggle" type="checkbox" />
+                    <label>{element}</label>
+                    <button className="destroy"></button>
+                  </div>
+                </li>
+              ))
+            }
           </ul>
         </section>
         <Footer />
--- src/header.js
+++ src/header.js
@@ -1,13 +1,34 @@
 
-function Header({inputValue}) {
+
+function Header({ inputValue, setInputValue, array, setArray }) {
+
+    const toDo = () => {
+        
+        setArray([...array, inputValue]);
+        console.log(array);
+    }
     return (
         <header className="header">
             <h1>todos</h1>
-            <form>
-            <input className="new-todo" value={inputValue} onChange={()=>{}} placeholder="What needs to be done?" autoFocus />
+            <form onSubmit={(e)=>{
+                e.preventDefault();
+                toDo()
+            } }>
+                <input
+                    className="new-todo"
+                    value={inputValue}
+
+                    onChange={(event) => {
+                      
+                        setInputValue(event.target.value)
+
+                    }}
+                    placeholder="What needs to be done?"
+                    autoFocus />
             </form>
         </header>
     )
 }
 
 export default Header;
+
